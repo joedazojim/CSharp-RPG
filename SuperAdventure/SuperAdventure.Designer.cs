42affdcecca83151ba82f8f5c8bcfbfbcefb05d8
oswaldo32s <oswaldo32gonzalez@gmail.com>
2026-05-05 12:09:43 -0600

    Resize window

--- SuperAdventure/SuperAdventure.Designer.cs
+++ SuperAdventure/SuperAdventure.Designer.cs
@@ -264,7 +264,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1695, 1482);
+            this.ClientSize = new System.Drawing.Size(742, 667);
             this.Controls.Add(this.dgvInventory);
             this.Controls.Add(this.dgvQuests);
             this.Controls.Add(this.rtbLocation);
